5b73f0fe857edbac475b0823b69bfce2dcba948b
karina-rodriguez <karminer60@gmail.com>
2020-06-07 16:57:03 -0700

    added code template for useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,32 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+
+
+/* logic
+  - state for the input ✅
+  - handleChanges ✅
+  - clearForm ✅
+
+  super form hook
+  - naming convention ✅
+  - any number of inputs in a form ✅
+  */
+
+export const useForm = (key, initialValue) => {
+  const [values, setValues] = useLocalStorage(key, initialValue);
+
+  const handleChanges = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const clearForm = e => {
+    e.preventDefault();
+    setValues(initialValue);
+  };
+
+  return [values, handleChanges, clearForm];
+};
